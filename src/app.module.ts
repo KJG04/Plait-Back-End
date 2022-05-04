1bcb0f83d8ffd68df5b6a60a7fee5488d93808e1
김진근 <freedom7113@gmail.com>
2022-05-04 09:10:22 +0900

    ⚙️ - aap module initial setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'plait',
+      entities: [],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      debug: true,
+      playground: true,
+      driver: ApolloDriver,
+      cors: {
+        origin: 'https://localhost:3000',
+        credentials: true,
+      },
+    }),
+  ],
 })
 export class AppModule {}

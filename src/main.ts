8e6cd4ecaffe5e645464f91ef57801a5238003f8
김진근 <freedom7113@gmail.com>
2022-05-12 09:47:36 +0900

    ⚙️ (#1) - setting cookies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.enableCors({ origin: 'https://localhost:3000', credentials: true });
+
   await app.listen(8080);
 
   if (module.hot) {

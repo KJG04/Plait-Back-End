36db4b208755ec7e4c0c5c7388dc474978fb15e6
김진근 <freedom7113@gmail.com>
2022-05-19 23:55:58 +0900

    🔌 (#1) - change constant folder name to shared

--- src/shared/subscriptionKeys.ts
+++ src/shared/subscriptionKeys.ts
@@ -1,6 +1,7 @@
 const enum subscriptionKeys {
   changeIsPlaying = 'CHANGE_IS_PLAYING',
   changeContents = 'CHANGE_CONTENTS',
+  listeningRoom = 'LISTENING_ROOM',
 }
 
 export default subscriptionKeys;

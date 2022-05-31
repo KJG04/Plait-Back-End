6183495e7a4979ba207bcd35d35a7af57ce1e718
김진근 <freedom7113@gmail.com>
2022-05-31 23:25:07 +0900

    🔌 (#1) - add dislistening task

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Room } from 'src/room/entities/room.entity';
 import { TaskService } from './task.service';
+import { User } from 'src/room/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room])],
+  imports: [TypeOrmModule.forFeature([Room, User])],
   providers: [TaskService],
   exports: [TypeOrmModule],
 })
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Room } from 'src/room/entities/room.entity';
+import { User } from 'src/room/entities/user.entity';
 import { LessThan, Repository } from 'typeorm';
 
 @Injectable()
@@ -9,6 +10,8 @@ export class TaskService {
   constructor(
     @InjectRepository(Room)
     private roomRepository: Repository<Room>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_6AM)
@@ -21,5 +24,17 @@ export class TaskService {
     });
 
     await this.roomRepository.remove(rooms);
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async dislisteningUser() {
+    const beforeOneMinute = new Date();
+    beforeOneMinute.setMinutes(beforeOneMinute.getMinutes() - 1);
+
+    const users = await this.userRepository.find({
+      where: { lastListeningAt: LessThan(beforeOneMinute), isListening: true },
+    });
+
+    await this.userRepository.save(users);
   }
 }
